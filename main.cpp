ee17b389aa4a9f068d0691ff81256fe150ac5f03
yuri <bigzyabra@gmail.com>
2025-09-04 21:34:22 +0300

    first commit

--- main.cpp
+++ main.cpp
@@ -1,186 +1,141 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <climits>
 using namespace std;
-<<<<<<< HEAD
-  
+
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-=======
-
-vector<pair<int, int>> generate_random_pairs(int n, int q) { //queries for test
-    vector<pair<int, int>> pairs;
+    cin.tie(0);
     
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<int> dist(1, n);
-    
-    for (int i = 0; i < q; i++) {
-        int a = dist(gen);
-        int b = dist(gen);
-        
-        // Если первое число больше второго - меняем местами
-        if (a > b) {
-            std::swap(a, b);
-        }
-        
-        pairs.emplace_back(a, b);
-    }
-    
-    return pairs;
-}
-
-
-string generate_random_string(int length) { //string for test
-    const char chars[] = {'a', 'b', 'c'};
-    string result;
-    
-    mt19937 generator(chrono::system_clock::now().time_since_epoch().count());
-    uniform_int_distribution<int> distribution(0, 2);
-    
-    for (int i = 0; i < length; ++i) {
-        result += chars[distribution(generator)];
-    }
-    
-    return result;
-}
-
-int my_solve(vector <int> &a_pref, vector <int> &b, vector <int> &c_suf, int &l, int &r) { //z solve
-    auto iterator_l = lower_bound(b.begin(), b.end(), l);
-    auto iterator_r = upper_bound(b.begin(), b.end(), r);
-    if (iterator_r == iterator_l) {
-        return 0;
-    }
-    int ans = 0;
-    int b_l = iterator_l - b.begin();
-    int b_r = iterator_r - b.begin() - 1;
-    while (b_r - b_l > 10) {
-        int mid1 = b_l + (b_r - b_l) / 3;
-        int mid2 = b_r - (b_r - b_l) / 3;
-        
-        int pos1 = b[mid1];
-        int a1 = a_pref[pos1-1] - a_pref[l-1];
-        int c1 = c_suf[pos1+1] - c_suf[r+1];
-        int val1 = min(a1, c1);
-        
-        int pos2 = b[mid2];
-        int a2 = a_pref[pos2-1] - a_pref[l-1];
-        int c2 = c_suf[pos2+1] - c_suf[r+1];
-        int val2 = min(a2, c2);
-        
-        if (val1 < val2) {
-            b_l = mid1;
-        } else {
-            b_r = mid2;
-        }
-    }
-    for (int i = b_l; i <= b_r; i++) {
-        int pos = b[i];
-        int a_count = a_pref[pos-1] - a_pref[l-1];
-        int c_count = c_suf[pos+1] - c_suf[r+1];
-        ans = max(ans, min(a_count, c_count));
-    }
-    return 2*ans + 1;
-}
-
-pair<int, int> brute_force_solve(vector <int>& a_pref, vector <int>& b, vector <int>& c_suf, int& l, int& r, string& s) { //brute force true solve
-    int ans = 0;
-    int working_b_pos;
-    bool marker = false;
-    for (int i = l; i <= r; i++) {
-        if (s[i-1] == 'b') {
-            marker = true;
-            int a_count = a_pref[i-1] - a_pref[l-1]; 
-            int c_count = c_suf[i+1] - c_suf[r+1];
-            if (min(couint_c, count_a) > ans) working_b_pos = i;
-            ans =  max(ans, min(c_count, a_count));
-        }
-    }
-    if(marker)return {2*ans + 1, working_b_pos};
-    return {0, -1};
-}
-
-int main(){
->>>>>>> c04f120 (returned working b for brute force)
     int n, q;
     cin >> n >> q;
     string s;
     cin >> s;
-    vector <int> a_pref(n+2, 0);
-    vector <int> c_suf(n+2, 0);
-    vector <int> b;
-    for (int i = 1; i <= n; i++) {
-        a_pref[i] = a_pref[i-1] + (s[i-1] == 'a');
-        if (s[i-1] == 'b') {
-            b.push_back(i);
+    
+    // Префиксные суммы для 'a', 'b', 'c'
+    vector<int> prefa(n + 1, 0), prefb(n + 1, 0), prefc(n + 1, 0);
+    vector<int> sufa(n + 2, 0), sufb(n + 2, 0), sufc(n + 2, 0);
+    
+    for (int i = 0; i < n; i++) {
+        prefa[i + 1] = prefa[i] + (s[i] == 'a');
+        prefb[i + 1] = prefb[i] + (s[i] == 'b');
+        prefc[i + 1] = prefc[i] + (s[i] == 'c');
+    }
+    
+    for (int i = n - 1; i >= 0; i--) {
+        sufa[i + 1] = sufa[i + 2] + (s[i] == 'a');
+        sufb[i + 1] = sufb[i + 2] + (s[i] == 'b');
+        sufc[i + 1] = sufc[i + 2] + (s[i] == 'c');
+    }
+    
+    // Для каждой позиции 'b' считаем разницу: (a слева) - (c справа)
+    vector<int> diff(n, INT_MAX);
+    vector<int> b_positions;
+    
+    for (int i = 0; i < n; i++) {
+        if (s[i] == 'b') {
+            b_positions.push_back(i);
+            int a_left = prefa[i + 1]; // a до текущей позиции (включительно)
+            int c_right = sufc[i + 1]; // c после текущей позиции (включительно)
+            diff[i] = a_left - c_right;
         }
     }
-    for (int i = n; i > 0; i--) {
-        c_suf[i] = c_suf[i+1] + (s[i-1] == 'c');
-    }
-<<<<<<< HEAD
+    
+    // Функция для поиска минимальной разницы на отрезке [l, r]
+    auto min_diff_in_range = [&](int l, int r) {
+        auto it_l = lower_bound(b_positions.begin(), b_positions.end(), l);
+        auto it_r = upper_bound(b_positions.begin(), b_positions.end(), r);
+        
+        if (it_l == it_r) return INT_MAX;
+        
+        int min_val = INT_MAX;
+        for (auto it = it_l; it != it_r; ++it) {
+            min_val = min(min_val, diff[*it]);
+        }
+        return min_val;
+    };
     
     while (q--) {
         int l, r;
         cin >> l >> r;
-        auto iterator_l = lower_bound(b.begin(), b.end(), l);
-        auto iterator_r = upper_bound(b.begin(), b.end(), r);
-        if (iterator_r == iterator_l) {
-            cout << 0 << endl;
+        l--; r--;
+        
+        int cnt_b = prefb[r + 1] - prefb[l];
+        if (cnt_b == 0) {
+            cout << "0\n";
             continue;
-=======
- 
-    while (q--){ //query processing
-        int l = queries[q].first;
-        int r = queries[q].second;
-
-        int my_ans = my_solve(pref_a, b_pos, suf_c, l, r);
-        pair <int,int> correct_ans = brute_force_solve(pref_a, b_pos, suf_c, l, r, s);
-
-        cout << "My answer: " << my_ans << ' ' << "Correct answer: " << correct_ans.first << ' ';
-
-        if (my_ans == correct_ans.first){
-            cout << "✅ Correct!" << endl;
->>>>>>> 2ad8cb2 (fixed q)
         }
-<<<<<<< HEAD
-        int ans = 0;
-        int b_l = iterator_l - b.begin();
-        int b_r = iterator_r - b.begin() - 1;
-        while (b_r - b_l > 10) {
-            int mid1 = b_l + (b_r - b_l) / 3;
-            int mid2 = b_r - (b_r - b_l) / 3;
-=======
-        else { //debug info
-            cout << "❌ Incorrect!" << endl;
-
-            cout << "Current query: " << l << ' ' << r << endl;
-            cout << "String segment ";
-            for (int i = l; i <= r; i++) cout << s[i - 1];
-            cout << endl << "Bruteforce B pos: " << correct_ans.second;
->>>>>>> c04f120 (returned working b for brute force)
+        
+        // Находим минимальную разницу на отрезке
+        int min_diff = min_diff_in_range(l, r);
+        if (min_diff == INT_MAX) {
+            cout << "0\n";
+            continue;
+        }
+        
+        // Количество 'a' слева от первой 'b' в отрезке
+        auto first_b_it = lower_bound(b_positions.begin(), b_positions.end(), l);
+        int first_b = *first_b_it;
+        int a_before_first_b = prefa[first_b + 1] - prefa[l];
+        
+        // Количество 'c' справа от последней 'b' в отрезке
+        auto last_b_it = upper_bound(b_positions.begin(), b_positions.end(), r);
+        if (last_b_it == b_positions.begin()) {
+            cout << "0\n";
+            continue;
+        }
+        last_b_it--;
+        int last_b = *last_b_it;
+        int c_after_last_b = prefc[r + 1] - prefc[last_b + 1];
+        
+        // Бинарный поиск по оптимальному k
+        int left = 0;
+        int right = min(prefa[r + 1] - prefa[l], prefc[r + 1] - prefc[l]);
+        int best_k = 0;
+        
+        while (left <= right) {
+            int mid = (left + right) / 2;
             
-            int pos1 = b[mid1];
-            int a1 = a_pref[pos1-1] - a_pref[l-1];
-            int c1 = c_suf[pos1+1] - c_suf[r+1];
-            int val1 = min(a1, c1);
+            // Проверяем, можно ли достичь разницы <= 0 с k = mid
+            // Минимальная требуемая разница: a_left - c_right <= 0
+            // где a_left + c_right = 2*mid
             
-            int pos2 = b[mid2];
-            int a2 = a_pref[pos2-1] - a_pref[l-1];
-            int c2 = c_suf[pos2+1] - c_suf[r+1];
-            int val2 = min(a2, c2);
+            bool possible = false;
             
-            if (val1 < val2) {
-                b_l = mid1;
+            // Вариант 1: используем первую 'b'
+            if (a_before_first_b >= mid && c_after_last_b >= mid) {
+                possible = true;
+            }
+            
+            // Вариант 2: проверяем минимальную разницу
+            if (!possible && min_diff <= 0) {
+                // Есть 'b' с отрицательной разницей - можно взять больше 'c'
+                possible = true;
+            }
+            
+            // Вариант 3: проверяем конкретные значения
+            if (!possible) {
+                // Ищем 'b' с минимальной разницей
+                for (int k = max(0, mid - 5); k <= min(mid + 5, right); k++) {
+                    if (min_diff <= k - (2 * mid - k)) {
+                        possible = true;
+                        break;
+                    }
+                }
+            }
+            
+            if (possible) {
+                best_k = mid;
+                left = mid + 1;
             } else {
-                b_r = mid2;
+                right = mid - 1;
             }
         }
-        for (int i = b_l; i <= b_r; i++) {
-            int pos = b[i];
-            int a_count = a_pref[pos-1] - a_pref[l-1];
-            int c_count = c_suf[pos+1] - c_suf[r+1];
-            ans = max(ans, min(a_count, c_count));
-        }
-        cout << 2*ans + 1 << endl;
+        
+        cout << (2 * best_k + 1) << "\n";
     }
+    
+    return 0;
 }
